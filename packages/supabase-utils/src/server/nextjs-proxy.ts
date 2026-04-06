3071f5d164d88b151d7a39887cdb702040986f40
DeloBelo123 <faragdelo@gmail.com>
2026-04-06 12:54:34 +0200

    updated nextjs-proxy description in supabase-utils

--- packages/supabase-utils/src/server/nextjs-proxy.ts
+++ packages/supabase-utils/src/server/nextjs-proxy.ts
@@ -3,13 +3,21 @@ import { createServerClient } from "@supabase/ssr";
 
 /**
  * WICHTIG: nutze im frontend einen 'createBrowserClient()' damit der sb-* token in den cookies gesetzt wird, !localStorage
+ * -> Kurze Erklärung:
+ * Dieser proxy server dient dazu, das wenn du mit einer 'createBrowserClient()' instanz das login und registrieren des 
+ * users machst und somit die sb-* tokens in die cookies kommen und nicht localStorage wie bei 'createClient()', genau diese
+ * tokens in den cookies abgecheckt werden, eine security massnnahme stattfindet ob die supabase-id nicht gespoofed ist
+ * sondern es wirklich der user ist, und ob der access-token noch gültig ist oder per refreshtoken der access-token erneurt
+ * werden soll. also -> createBrowserClient() -> sb-* tokens in cookies -> proxy server -> supabase.auth.getClaims() -> 
+ * im server frische sb-daten weil dieser proxy frisch-gecheckte cookies weiterleitet!
  * WICHTIG: du musst im root von source eine proxy.ts file haben die genau so aussieht:
- * @example import { NextRequest } from "next/server";
+ * @example 
+ *  import { NextRequest } from "next/server";
     import { supabaseAuthProxy } from "@delofarag/supabase-utils";
 
     // (copy-paste this):
     export async function proxy(request: NextRequest) {
-    return supabaseAuthProxy(request);
+      return supabaseAuthProxy(request);
     }
     
     // (copy-paste this):
