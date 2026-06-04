dea97cdd99a1397f3163963bb8fb35ed3186c39a
DeloBelo123 <faragdelo@gmail.com>
2026-06-04 15:53:30 +0200

    before Hermesagent and automations implementation

---
+++ packages/ai-utils/src/heart/hermesAgent.ts
@@ -0,0 +1,10 @@
+const lokalUrl = "http://localhost:8787/v1" 
+interface HermesAgentInit {
+    url?: string
+    apikey?: string // selbst erstellter apikey der einfach geprüft wird
+}
+export class HermesAgent {
+    constructor(){
+
+    }
+}
