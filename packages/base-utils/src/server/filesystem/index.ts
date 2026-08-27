21e5b62820e9d0905dae7f97bffc47bb24f87abb
DeloBelo123 <faragdelo@gmail.com>
2026-08-27 16:30:09 +0200

    feat(base-utils): FilePath und DirPath als Alias auf File und Directory
    Beide Namen zeigen auf dieselbe Klasse. Damit bricht Code aus 1.4.0, der
    noch FilePath/DirPath importiert, durch die Umbenennung nicht, und wer das
    globale File nicht verdecken will, kann weiter den langen Namen nehmen.

--- packages/base-utils/src/server/filesystem/index.ts
+++ packages/base-utils/src/server/filesystem/index.ts
@@ -1,3 +1,19 @@
 export * from "./types.js"
 export * from "./funcs.js"
 export * from "./classes.js"
+
+/**
+ * Die alten Namen aus Version <= 1.4.0 als Alias.
+ *
+ * Beide Namen zeigen auf dieselbe Klasse - der Aufrufer entscheidet, ob er
+ * das kurze `File` will (verdeckt in seiner Datei Node's globales `File`)
+ * oder das eindeutige `FilePath`.
+ *
+ * @example
+ * ```ts
+ * import { File, Directory } from "@delofarag/base-utils/server"
+ * import { FilePath, DirPath } from "@delofarag/base-utils/server"
+ * import * as fsx from "@delofarag/base-utils/server"   // fsx.File verdeckt nichts
+ * ```
+ */
+export { File as FilePath, Directory as DirPath } from "./classes.js"
